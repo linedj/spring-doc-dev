73ebdcc56a18eba7bdef59da23f05598c9598af7
linedj <eunjun998@gmail.com>
2025-02-12 14:17:20 +0900

    devInitData

--- backend/src/main/java/com/example/spring_doc/global/springDoc/SpringDocConfig.java
+++ backend/src/main/java/com/example/spring_doc/global/springDoc/SpringDocConfig.java
@@ -1,13 +1,21 @@
 package com.example.spring_doc.global.springDoc;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "API 서버", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "baerer",
+        bearerFormat = "JWT"
+)
 public class SpringDocConfig {
 
     @Bean
